371a4c54c8c962830cdc80a9867ee9534ce858a2
Ada Cohen <ada@fieldbook.com>
2022-02-22 15:56:26 -0500

    Fix run script

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -euo pipefail
+
 cd "$(dirname "$0")"
 go build .
 
 # Loop forever to restart the server even if it panics
 while true; do
   echo "Restarting server..."
-  ./coordinate 80
+  sudo ./coordinate 80
   sleep 1
 done
